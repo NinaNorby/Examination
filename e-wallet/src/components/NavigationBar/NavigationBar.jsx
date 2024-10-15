ca5c2d624aeec14cde2bce9f87e23dfec0391561
NinaNorby <norbyy_@hotmail.com>
2024-10-15 08:36:35 +0200

    20241015 - tog bort Add card i navbar

--- e-wallet/src/components/NavigationBar/NavigationBar.jsx
+++ e-wallet/src/components/NavigationBar/NavigationBar.jsx
@@ -12,9 +12,8 @@ function NavigationsBar() {
     return (
         <>
             <nav className={styles["navbar"]}>
-                <Link to="/addcard"className={styles.link}>Add a card</Link>
-                <Link to="/"className={styles.link}>Home</Link>
-                <Link to="/settings"className={styles.link}>Settings</Link>
+                <Link to="/" className={styles.link}>Home</Link>
+                <Link to="/settings" className={styles.link}>Settings</Link>
 
 
             </nav>
